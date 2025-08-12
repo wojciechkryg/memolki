4662f412064eff983515b427bac10310c5bde12b
Wojciech Kryg <wojciech.kryg@thefabulous.co>
2025-08-13 00:23:49 +0200

    fix statusbar color

--- app/src/main/java/com/wojdor/memolki/app/AppActivity.kt
+++ app/src/main/java/com/wojdor/memolki/app/AppActivity.kt
@@ -1,7 +1,9 @@
 package com.wojdor.memolki.app
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.activity.SystemBarStyle
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
@@ -14,7 +16,12 @@ class AppActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
-        enableEdgeToEdge()
+        enableEdgeToEdge(
+            statusBarStyle = SystemBarStyle.light(
+                Color.TRANSPARENT,
+                Color.TRANSPARENT
+            )
+        )
         setContent {
             AppTheme {
                 AppNavigation()
