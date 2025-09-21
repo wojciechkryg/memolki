9b2df3fd7a2452db95f93761863e8b8983bf9f48
Wojciech Kryg <wojciech.kryg@thefabulous.co>
2025-09-22 01:49:51 +0200

    fix build

--- app/src/main/java/com/wojdor/memolki/di/coroutine/CoroutineModule.kt
+++ app/src/main/java/com/wojdor/memolki/di/coroutine/CoroutineModule.kt
@@ -4,6 +4,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 
 @Module
@@ -12,13 +13,13 @@ object CoroutineModule {
 
     @DefaultDispatcher
     @Provides
-    fun provideDefaultDispatcher() = Dispatchers.Default
+    fun provideDefaultDispatcher(): CoroutineDispatcher = Dispatchers.Default
 
     @IoDispatcher
     @Provides
-    fun provideIoDispatcher() = Dispatchers.IO
+    fun provideIoDispatcher(): CoroutineDispatcher = Dispatchers.IO
 
     @MainDispatcher
     @Provides
-    fun provideMainDispatcher() = Dispatchers.Main
+    fun provideMainDispatcher(): CoroutineDispatcher = Dispatchers.Main
 }
--- app/src/main/java/com/wojdor/memolki/util/media/CardPairMatchedPlayer.kt
+++ app/src/main/java/com/wojdor/memolki/util/media/CardPairMatchedPlayer.kt
@@ -2,15 +2,17 @@ package com.wojdor.memolki.util.media
 
 import android.content.Context
 import com.wojdor.memolki.R
+import com.wojdor.memolki.di.coroutine.MainDispatcher
 import com.wojdor.memolki.domain.usecase.GetSettingsUseCase
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineDispatcher
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class CardPairMatchedPlayer @Inject constructor(
-    context: Context,
-    coroutineDispatcher: CoroutineDispatcher,
+    @ApplicationContext context: Context,
+    @MainDispatcher coroutineDispatcher: CoroutineDispatcher,
     getSettingsUseCase: GetSettingsUseCase
 ) : SoundPlayer(context, coroutineDispatcher, getSettingsUseCase) {
 
