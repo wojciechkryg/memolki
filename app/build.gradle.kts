a3cd5c98d23232acee7ed060ff280b6e126430f3
Wojciech Kryg <wojciech.kryg@thefabulous.co>
2025-11-02 23:23:05 +0100

    fix version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.wojdor.memolki"
         minSdk = 23
         targetSdk = 36
-        versionCode = 3
-        versionName = "0.0.3"
+        versionCode = 5
+        versionName = "0.0.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
