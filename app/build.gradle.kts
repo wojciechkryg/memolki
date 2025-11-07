548ce4e22eefb5b52d3e5068548a088dc23c9645
Wojciech Kryg <wojciech.kryg@thefabulous.co>
2025-11-07 23:38:27 +0100

    bump version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -35,8 +35,8 @@ android {
         applicationId = "com.wojdor.memolki"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1000002
-        versionName = "1.0.2"
+        versionCode = 1000003
+        versionName = "1.0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
