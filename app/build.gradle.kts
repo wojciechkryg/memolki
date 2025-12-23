503d83a7426ab98f2c7d62eb1d5ee2e4c3ea8e4d
Wojciech Kryg <krygwojciech@gmail.com>
2025-12-23 07:01:44 +0100

    Chore/version bump 1.3.5 (#73)
    Automates version bump to 1.3.5
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
      * App version updated to 1.3.5
    
    <sub>✏️ Tip: You can customize this high-level summary in your review
    settings.</sub>
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    Co-authored-by: wojciechkryg <19477495+wojciechkryg@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.wojdor.memolki"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1003004
-        versionName = "1.3.4"
+        versionCode = 1003005
+        versionName = "1.3.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
