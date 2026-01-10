880b87bb08efdb123be8dadd560fa7c6f9860251
wojciechkryg <19477495+wojciechkryg@users.noreply.github.com>
2026-01-10 08:22:18 +0000

    Bump version to 1.3.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.wojdor.memolki"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1003006
-        versionName = "1.3.6"
+        versionCode = 1003007
+        versionName = "1.3.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
