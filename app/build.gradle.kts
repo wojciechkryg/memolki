5590f9dd58e5139ccecd80e41618c5657fae0bf9
wojciechkryg <19477495+wojciechkryg@users.noreply.github.com>
2026-03-17 20:14:28 +0000

    Bump version to 1.3.12

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.wojdor.memolki"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1003011
-        versionName = "1.3.11"
+        versionCode = 1003012
+        versionName = "1.3.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
