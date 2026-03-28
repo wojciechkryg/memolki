d3c6521003f485f782ae386a1c9fb2d1905beb1b
Wojciech Kryg <krygwojciech@gmail.com>
2026-03-28 12:34:27 +0100

    Chore/version bump 1.3.16 (#104)
    Automates version bump to 1.3.16
    
    <!-- This is an auto-generated comment: release notes by coderabbit.ai
    -->
    
    ## Summary by CodeRabbit
    
    * **Chores**
      * Updated app version to 1.3.16.
    
    <!-- end of auto-generated comment: release notes by coderabbit.ai -->
    
    Co-authored-by: wojciechkryg <19477495+wojciechkryg@users.noreply.github.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.wojdor.memolki"
         minSdk = 23
         targetSdk = 36
-        versionCode = 1003015
-        versionName = "1.3.15"
+        versionCode = 1003016
+        versionName = "1.3.16"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
